27e6222bab0e919d19dc7c10ffe6afff42b78c5f
shullmb <dev.shullmb@gmail.com>
2015-10-08 19:32:11 -0700

    LineAnalyzers complete, Solution fails 3 tests

--- course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
+++ course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
@@ -1,33 +1,15 @@
-#Implement all parts of this assignment within (this) module2_assignment2.rb file
-
-#Implement a class called LineAnalyzer.
 class LineAnalyzer
-  #Implement the following read-only attributes in the LineAnalyzer class. 
-  #* highest_wf_count - a number with maximum number of occurrences for a single word (calculated)
-  #* highest_wf_words - an array of words with the maximum number of occurrences (calculated)
-  #* content,         - the string analyzed (provided)
-  #* line_number      - the line number analyzed (provided)
   attr_reader :highest_wf_count, :highest_wf_words, :content, :line_number, :dictionary
-  #Add the following methods in the LineAnalyzer class.
-  #* initialize() - taking a line of text (content) and a line number
-  #* calculate_word_frequency() - calculates result
 
-  #Implement the initialize() method to:
-  #* take in a line of text and line number
-  #* initialize the content and line_number attributes
-  #* call the calculate_word_frequency() method.
   def initialize(content, line_number)
     @content = content
     @line_number = line_number
     @dictionary = Hash.new(0)
+    @highest_wf_words = []
     calculate_word_frequency
     
   end
-  #Implement the calculate_word_frequency() method to:
-  #* calculate the maximum number of times a single word appears within
-  #  provided content and store that in the highest_wf_count attribute.
-  #* identify the words that were used the maximum number of times and
-  #  store that in the highest_wf_words attribute.
+
   def create_dictionary
     freq = self.content.split
     freq.each do |word|
@@ -46,14 +28,16 @@ class LineAnalyzer
   end
 end
 
-#  Implement a class called Solution. 
 class Solution
 
   # Implement the following read-only attributes in the Solution class.
   #* highest_count_across_lines - a number with the value of the highest frequency of a word
   #* highest_count_words_across_lines - an array with the words with the highest frequency
-  attr_reader :highest_count_across_lines, :highest_count_words_across_lines
-  attr_accessor :analyzers
+  attr_reader :analyzers, :highest_count_across_lines, :highest_count_words_across_lines
+  
+  def initialize
+      @analyzers = []
+  end
   # Implement the following methods in the Solution class.
   #* analyze_file() - processes 'test.txt' intro an array of LineAnalyzers
   #* calculate_line_with_highest_frequency() - determines which line of
@@ -61,29 +45,31 @@ class Solution
   #* print_highest_word_frequency_across_lines() - prints the words with the 
   #highest number of occurrences and their count
   def analyze_file()
-    @analyzers = Array.new
 
     lines = File.readlines("test.txt")
 
     lines.each_with_index do |content, line_number|
-      @analyzers << LineAnalyzer.new(content, line_number)
+      analyzer = LineAnalyzer.new(content, line_number)
+      self.analyzers << analyzer.content.split
     end  
 
-    #[LineAnalyzer.new("test.txt", 1), LineAnalyzer.new("test.txt", 2), LineAnalyzer.new("test.txt", 3)]
+    
   end
   
   def calculate_line_with_highest_frequency()
+    # dictionary = Hash.new(0)
+    # self.analyzers.each do |word|
+    #   dictionary[word] += 1
+    # end
+    # sorted_dictionary = Hash.new[dictionary.sort_by {|k,v| v }]
+    # @highest_count_across_lines = sorted_dictionary.values.max
+    # @highest_count_words_across_lines = sorted_dictionary.keys.max_by
 
   end
 
   def print_highest_word_frequency_across_lines()
-    a = File.readlines("text.txt").split
-    count = Hash.new(0)
-    a.each do |x|
-      count[x] += 1
-    end
-    sorted_dictionary = Hash.new[count.sort_by {|k,v| v }]
-    @highest_count_words_across_lines = sorted_dictionary.keys.max_by
+    puts "The following words have the highest word frequency per line:"
+    puts ""
   end
   # Implement the analyze_file() method() to:
   #* Read the 'test.txt' file in lines 
