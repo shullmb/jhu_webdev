978342b2a1c5eb4f305094b68c60c7cde792e9cc
shullmb <dev.shullmb@gmail.com>
2015-10-09 05:30:43 -0700

    rspec: 18 passed, 1 failure

--- course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
+++ course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
@@ -33,54 +33,42 @@ class Solution
   # Implement the following read-only attributes in the Solution class.
   #* highest_count_across_lines - a number with the value of the highest frequency of a word
   #* highest_count_words_across_lines - an array with the words with the highest frequency
-  attr_reader :analyzers, :highest_count_across_lines, :highest_count_words_across_lines
+  attr_accessor :analyzers, :highest_count_across_lines, :highest_count_words_across_lines
   
   def initialize
       @analyzers = []
   end
-  # Implement the following methods in the Solution class.
-  #* analyze_file() - processes 'test.txt' intro an array of LineAnalyzers
-  #* calculate_line_with_highest_frequency() - determines which line of
-  #text has the highest number of occurrence of a single word
-  #* print_highest_word_frequency_across_lines() - prints the words with the 
-  #highest number of occurrences and their count
+
   def analyze_file()
 
     lines = File.readlines("test.txt")
 
     lines.each_with_index do |content, line_number|
       analyzer = LineAnalyzer.new(content, line_number)
-      self.analyzers << analyzer.content.split
+      self.analyzers << analyzer
     end  
 
     
   end
   
   def calculate_line_with_highest_frequency()
-    # dictionary = Hash.new(0)
-    # self.analyzers.each do |word|
-    #   dictionary[word] += 1
-    # end
-    # sorted_dictionary = Hash.new[dictionary.sort_by {|k,v| v }]
-    # @highest_count_across_lines = sorted_dictionary.values.max
-    # @highest_count_words_across_lines = sorted_dictionary.keys.max_by
+    highest_wf = 0
+    words = []
+    analyzers.each_with_index do |analyzer, line_number|
 
+      if analyzer.highest_wf_count > highest_wf
+        highest_wf = analyzer.highest_wf_count
+      end
+      
+      words << analyzer.highest_wf_words
+    end
+
+    self.highest_count_across_lines = highest_wf
   end
 
   def print_highest_word_frequency_across_lines()
     puts "The following words have the highest word frequency per line:"
     puts ""
   end
-  # Implement the analyze_file() method() to:
-  #* Read the 'test.txt' file in lines 
-  #* Create an array of LineAnalyzers for each line in the file
 
-  # Implement the calculate_line_with_highest_frequency() method to:
-  #* calculate the highest number of occurences of a word across all lines
-  #and stores this result in the highest_count_across_lines attribute.
-  #* identifies the words that were used with the highest number of occurrences
-  #and stores them in print_highest_word_frequency_across_lines.
-
-  #Implement the print_highest_word_frequency_across_lines() method to
-  #* print the result in the following format
 end
