c9754eabe9811f5101bd9beef666a38a46954d62
shullmb <dev.shullmb@gmail.com>
2015-10-18 12:35:42 -0700

    http-client complete recipehunter started

--- http_client/student-start/module3_1_assignment.rb
+++ http_client/student-start/module3_1_assignment.rb
@@ -1,5 +1,17 @@
 require 'httparty'
 
+ENV["FOOD2FORK_KEY"] = ''
+
+
 class Recipe
+	include HTTParty
+	base_uri 'http://food2fork.com/api'
+	default_params[:key] = ENV["FOOD2FORK_KEY"]
+	format :json
+
+	def self.for term 
+		get("/search", query: { q: term}) ["recipes"]
+	end
+
 
 end
