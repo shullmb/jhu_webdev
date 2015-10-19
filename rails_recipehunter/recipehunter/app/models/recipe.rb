5991a73b5d165146bf0f7ecb22cf3c7858e6bf09
shullmb <dev.shullmb@gmail.com>
2015-10-18 17:46:13 -0700

    Recipe Hunter ready for submission, passes all rspec tests

--- rails_recipehunter/recipehunter/app/models/recipe.rb
+++ rails_recipehunter/recipehunter/app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe
   include HTTParty
 
   key_value = ENV["FOOD2FORK_KEY"]
-  hostport =  'www.food2fork.com' #ENV['FOOD2FORK_SERVER_AND_PORT'] ||
+  hostport =  ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com' 
   base_uri "http://#{hostport}/api"   
   default_params key: key_value
   format :json
