a23039b90e82a26474007cafe2f5b01e21b5096c
shullmb <dev.shullmb@gmail.com>
2015-11-08 17:23:00 -0800

    seeds.rb fixed

--- ar_crud/todolists/db/schema.rb
+++ ar_crud/todolists/db/schema.rb
@@ -40,9 +40,12 @@ ActiveRecord::Schema.define(version: 20151108154025) do
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
+    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
--- ar_crud/todolists/db/seeds.rb
+++ ar_crud/todolists/db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
+
+
+deadline = Date.today + 1.year
+
+Profile.create!([
+{ first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female" },
+{ first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male" },
+{ first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male" },
+{ first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female" }])
+
+profiles = Profile.all
+
+profiles.each do |profile|
+  profile.create_user( username: profile.last_name, password_digest: "!#{profile.first_name}#{profile.birth_year[3]}" )
+  profile.user.todo_lists.create!(list_name: "#{profile.first_name}'s List", list_due_date: deadline )
+end
+
+todolists = TodoList.all
+
+tasks = ["Snuggle a Mal", "Hug a Char", "Clean bathroom","Put away laundry", "Finish homework"]
+
+todolists.each do |list|
+  5.times do
+    list.todo_items.create!(due_date: deadline, title: "Task", description: tasks.sample)
+  end
+end
