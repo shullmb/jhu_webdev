96c5d2614f969885b333686c93b2f2bd4dfc25ab
shullmb <dev.shullmb@gmail.com>
2015-11-22 22:01:14 -0800

    passing

--- auth/todolists/app/controllers/sessions_controller.rb
+++ auth/todolists/app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
-class SessionsController < ApplicationController
+class SessionsController < ApplicationController	
+  skip_before_action :ensure_login, only: [:new, :create]
+
   def new
   end
 
   def create
-      user = User.find_by(username: params[:user][:username])
-			password = params[:user][:password]
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
 
-			if user && user.authenticate(password)
-				session[:user_id] = user.id
-				redirect_to root_path, notice: "Logged in successfully"
-			else
-				redirect_to login_path, alert: "Invalid username/password combination"
-			end
+    if user && user.authenticate(password)
+       session[:user_id] = user.id
+       redirect_to root_path, notice: "Logged in successfully"
+    else
+       redirect_to login_path, alert: "Invalid username/password combination"
+    end
   end
 
   def destroy
-    reset_session # wipe out session
-    redirect_to login_path, notice: "Logged out successfully"
+    reset_session
+    redirect_to login_path, notice: "You have been logged out"
   end
 end
